06285a4479ac04cfeaa83aaff84ef600f7702458
Lyle <ylese.jose@gmail.com>
2024-09-20 03:48:05 +1200

    Fixed BowlingGame.py
    - fixed incorrect logic in frame counting
    - fixed incorrect function name (typo)
    - fixed improper strike handling
    - refactored frame logic in score( )

--- BowlingGame.py
+++ BowlingGame.py
@@ -1,30 +1,42 @@
 class BowlingGame:
     def __init__(self):
-        self.rolls=[]
-    def roll(self,pins):
-        self.rolls.append(pins)
+        self.rolls = []  #stores the number of pins knocked down in each roll
+    
+    def roll(self, pins):
+        self.rolls.append(pins)  #add the pins knocked down in each roll
+    
     def score(self):
         result = 0
-        rollIndex=0
+        rollIndex = 0
         for frameIndex in range(10):
-            if frameIndex in range(10):
-                result += self.StrikeScore(rollIndex)
-                rollIndex +=1
-            elif self.isSpare(rollIndex):
+            #check for strike
+            if self.isStrike(rollIndex):  
+                result += self.strikeScore(rollIndex)
+                rollIndex += 1  
+            elif self.isSpare(rollIndex):  #check for spare
                 result += self.spareScore(rollIndex)
-                rollIndex +=2
-            else:
+                rollIndex += 2  
+            else:  #regular frame (no strike or spare)
                 result += self.frameScore(rollIndex)
-                rollIndex +=2
+                rollIndex += 2  
         return result
     
+    #check if the roll is a strike
     def isStrike(self, rollIndex):
         return self.rolls[rollIndex] == 10
+    
+    #check if the frame is a spare
     def isSpare(self, rollIndex):
-        return self.rolls[rollIndex]+ self.rolls[rollIndex+1]==10
-    def stickeScore(self,rollIndex):
-        return 10+ self.rolls[rollIndex+1]+ self.rolls[rollIndex+2]
-    def spareScore(self,rollIndex):
-        return 10+ self.rolls[rollIndex+2]
+        return self.rolls[rollIndex] + self.rolls[rollIndex + 1] == 10
+    
+    #calculate the score for a strike
+    def strikeScore(self, rollIndex):
+        return 10 + self.rolls[rollIndex + 1] + self.rolls[rollIndex + 2]
+    
+    #calculate the score for a spare
+    def spareScore(self, rollIndex):
+        return 10 + self.rolls[rollIndex + 2]
+    
+    #calculate the score for a normal frame (no strike or spare)
     def frameScore(self, rollIndex):
         return self.rolls[rollIndex] + self.rolls[rollIndex + 1]
